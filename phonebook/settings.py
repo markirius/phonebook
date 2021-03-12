b0aa522328c6c551bcecf74195cd74ff7240e183
markirius <markbombomb@gmail.com>
2021-03-12 19:18:55 -0300

    added env vars for secret_key and debug

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -12,19 +12,28 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import environ
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES_DIRS = os.path.join(BASE_DIR, "templates")
 
+# Setting environ
+env = environ.Env(
+        DEBUG=(bool, False)
+        )
+
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9&!&@^fvpg84&^dk237*k$(311s7!3it)=8wd(kyww7=#xx9ht'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
