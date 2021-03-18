3dccfdcec18662a3b82b8b70b0efabbda8d589bc
markirius <markbombomb@gmail.com>
2021-03-18 10:17:59 -0300

    changed psycopg2-binary to psycopg2 and templates updates

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contacts(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=254, blank=True, unique=True)
+    email = models.EmailField(max_length=254, blank=True)
 
     class Meta:
         verbose_name = "Contact"
