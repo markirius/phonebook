86ef4730c454176daeda796274a9c6a86ca1e708
markirius <markbombomb@gmail.com>
2021-03-19 12:58:33 -0300

    resolve language configuration with .env file

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "pt-br"
+LANGUAGE_CODE = config("LANGUAGE_CODE", default="pt-br")
 
 LANGUAGES = [
         ("pt-br", "Brazilian Portuguese"),
