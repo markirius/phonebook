abddaf164ce38bc9be8eabcd346425daf24041bb
markirius <markbombomb@gmail.com>
2021-03-25 19:15:51 -0300

    base form template create, added error messages on forms

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from apps.core.views import SignUpView, PasswordChangeView
+from apps.core.views import PasswordChangeView, SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("password_change", PasswordChangeView.as_view(), name="password_change")
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(),
+        name="password_change"
+    )
 ]
